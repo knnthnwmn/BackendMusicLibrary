7128173eb02e39d894b5583f06e8b4c55bc0e700
Kenneth Newman <kennethnewman80@gmail.com>
2021-11-10 19:01:57 -0600

    create app.post

--- Index.js
+++ Index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const repoContext = require('./repository/repository-wrapper');
-const { validateSongs } = require('./middleware/songs-validation');
+
 
 
 const app = express(); 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+
 
 
 app.listen(3000, function () { 
@@ -17,29 +18,9 @@ app.get('/api/songs', (req, res) => {
     const songs = repoContext.songs.findAllSongs();
     return res.send(songs);
 });
-   
 
-app.post('/api/songs', [validateSongs], (req, res) => {
-    const newSongs = req.body;
-    const addedSongs = repoContext.songs.createSongs(newSongs);
-    return res.send(addedSongs);
-});
-   
-
-app.put('/api/songs/:id', [validateSongs], (req, res) => {
-    const id = req.params.id;
-    const songsPropertiesToUpdate = req.body;
-    const updatedSongs = repoContext.songs.updateSongs(id, songsPropertiesToUpdate);
-    return res.send(updatedSongs)
-});
-   
-app.delete('/api/songs/:id', (req, res) => {
-    const id = req.params.id;
-    const updatedDataSet = repoContext.songs.deleteSongs(id);
-    return res.send(updatedDataSet);
-   });
-   
-   
-
-
-    
+app.post('/api/songs', (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.send(addedSong);
+    });    
